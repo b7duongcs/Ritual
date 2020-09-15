74b0677ed728003ee9e2b165ce632437aaf6da00
Brian Duong <b7duong@uwaterloo.ca>
2020-09-15 15:50:49 -0400

    Update profilescreen.dart

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -7,6 +7,30 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Profile'),
       ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Icon(Icons.account_circle, size: 100.0)],
+          ),
+          Text('Name'),
+          Card(
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('Profile'),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ))),
+          SwitchListTile(
+              title: const Text('Dark Mode'), value: false, onChanged: null),
+        ],
+      ),
     );
   }
 }
